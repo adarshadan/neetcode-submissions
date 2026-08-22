164511548c89526605055e1be16de53fcb65b954
adarshadan <163655347+adarshadan@users.noreply.github.com>
2026-08-22 12:31:08 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.py
@@ -0,0 +1,14 @@
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        list_size = len(nums)
+
+        unique_elements = len(set(nums))
+
+        if list_size < 0 or list_size > 10**5:
+            raise "Invalid List Size"
+        else:
+            if unique_elements != list_size:
+                return True
+            else:
+                return False
