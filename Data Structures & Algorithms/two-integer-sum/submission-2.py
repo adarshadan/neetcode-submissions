fe1159a1a80342578449bc6e86b3a3b026629811
adarshadan <163655347+adarshadan@users.noreply.github.com>
2026-08-27 10:59:06 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_indices = []
+        isMatchFound = False
+        for i,value1 in enumerate(nums):
+            for j,value2 in enumerate(nums):
+                if i!=j:
+                    if value1 + value2 == target:
+                        num_indices.append(i)
+                        num_indices.append(j)
+                        isMatchFound = True
+                        print("Match Found")
+                        break
+            if isMatchFound:
+                break
+        return num_indices
